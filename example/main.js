7fc573070318890c83395a5da13696d7985e4b87
Ian Usher <ian@codeplay.com>
2019-05-20 15:30:43 +0100

    Cleaning up the examples a bit

--- example/main.js
+++ example/main.js
@@ -4,21 +4,10 @@ require.config({
         'malicacid': ['malicacid.bundle'],
         'malicacidcss': ['malicacidcss.bundle'],
         'jquery': ['https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min'],
-        'jquery-ui': ['https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min'],
-        'underscore': ['https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min'],
-        'backbone': ['https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.4.0/backbone-min'],
-        'text': ['https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min'],
-        'domReady': ['./domReady']
-    },
-    shim: {
-        'jquery-ui': ['jquery'],
-        'underscore': {
-            exports: '_'
-        }
     }
 });
 
-require(["malicacid"], function(malicAcid){
+require(["jquery", "malicacid", "malicacidcss"], function($, malicAcid){
     class DeviceStatusForm extends malicAcid.BasicFormWithGenerators{
         constructor(options){
             super(options);
@@ -45,12 +34,12 @@ require(["malicacid"], function(malicAcid){
         }
     }
 
-    const form = new DeviceStatusForm({form: document.getElementsByTagName("form")[0]});
-    document.getElementById("submit-this").addEventListener("click", function(e){
+    const form = new DeviceStatusForm({form: $("form")});
+    $("#submit-this").on("click", function(e){
         console.log(form.getFormData());
     }.bind(this));
-    document.getElementById("populate-form").addEventListener("click", function(e){
-        let content = document.getElementById("load-entry").innerHTML;
+    $("#populate-form").on("click", function(e){
+        let content = $("#load-entry").text();
         console.log(content);
         let data = JSON.parse(content);
         form.reset();
